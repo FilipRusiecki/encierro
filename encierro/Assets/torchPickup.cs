17c2587ca8f78f72258833389d22a8d92731529b
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2022-12-05 12:24:18 +0000

    RWMPB22-80 Added torchPickup which activates the playersTorch

---
+++ encierro/Assets/torchPickup.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torchPickup : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public int speed;
+    public int bounds;
+
+    public GameObject playersTorch;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        if (transform.position.x < bounds)
+        {
+            transform.position = new Vector3(13, transform.position.y, transform.position.z);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            playersTorch.SetActive(true);
+        }
+
+    }
+}
